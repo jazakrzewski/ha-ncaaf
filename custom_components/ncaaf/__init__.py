5ba8253767bdcca8d07c68483352f39c5b9b3207
jazakrzewski <58450237+jazakzewski@users.noreply.github.com>
2022-09-03 19:03:37 -0400

    Fix Team ID Match

--- custom_components/ncaaf/__init__.py
+++ custom_components/ncaaf/__init__.py
@@ -165,12 +165,12 @@ async def async_get_state(config) -> dict:
                     team_index = 0
                     found_team = True
                 else:
-                    if event["competitions"][0]["competitors"][0]["team"]["abbreviation"] == team_id:
+                    if event["competitions"][0]["competitors"][1]["team"]["abbreviation"] == team_id:
                         team_index = 1
                         found_team = True
                     else:
                         found_team = False
-            if found_team == True
+            if found_team == True:
                 _LOGGER.debug("Found team; parsing data.")
                 oppo_index = abs((team_index-1))
                 values["state"] = event["status"]["type"]["state"].upper()
